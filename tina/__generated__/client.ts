6cffba02eb94750e10a9d8b68ba0f8cf2fcb68ee
japcode <japcrowley.thetraveler@gmail.com>
2026-07-09 15:16:12 -0400

    ignore generated tina files

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types.js";
-export const client = createClient({ cacheDir: '/home/japcode/PersonalProjects/ula-social-comunication/tina/__generated__/.cache/1783621793171', url: 'http://localhost:4001/graphql', token: '', queries,  });
-export default client;
-  
